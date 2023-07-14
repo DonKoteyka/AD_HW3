d22727f9c46005ae63575fb9be43542224c6996f
Don Koteyka <don.kotejka@yandex.ru>
2023-07-14 19:47:31 +0300

    release №1

--- main.py
+++ main.py
@@ -2,32 +2,25 @@ import os
 import json
 import time
 import requests
+
 import bs4
 import fake_headers
-
-
-
 
 def write_json(dict, dir = os.getcwd(), json_name = 'result.json'):
     with open(f'{dir}/{json_name}', 'w', encoding='utf-8') as f:
         json.dump(dict, f, indent=4, sort_keys=True, ensure_ascii=False)
 
-words_search = ','.join(('Django','Flask',))
+words_search = ' '.join(('Django','Flask',))
 region = (1, 2,)
-page = 1
-'''
-'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
-'''
 
 main_url = 'https://spb.hh.ru/search/vacancy'
 
-
 def main():
-    pages = 1
+    pages = 0
     result = list()
     while True:
 
-        headers = fake_headers.Headers(browser='firefox', os='win')
+        headers = fake_headers.Headers(browser="chrome", os="win")
         headers_dict = headers.generate()
         params = {
             'text':words_search,
@@ -36,42 +29,38 @@ def main():
             'search_period':0,
             'page':pages
         }
-
         response = requests.get(main_url, headers=headers_dict, params=params)
         main_html = bs4.BeautifulSoup(response.text, "lxml")
 
-        articles_tags = main_html.find_all(class_ = 'serp-item')
+        main_articles = main_html.find("div", id="a11y-main-content")
+        articles_tags = main_articles.find_all("div", class_="serp-item", limit = 50)
 
         for article_tag in articles_tags:
-            temp_res = list()
-            link = article_tag.find(class_ = 'serp-item__title').get('href')
-            company = article_tag.find('a', class_ = 'bloko-link bloko-link_kind-tertiary').get_text()
 
-            address = list(article_tag.find(class_="vacancy-serp-item__info").children)[1].text
+            link = article_tag.find(class_ = 'serp-item__title').get('href')
+            company = article_tag.find('a', class_ = 'bloko-link bloko-link_kind-tertiary').get_text().replace(' ', ' ')
+            address = list(article_tag.find(class_="vacancy-serp-item__info").children)[1].text.replace(' ', ' ')
             if article_tag.find('span', class_ = 'bloko-header-section-3'):
-                salary = article_tag.find('span', class_ = 'bloko-header-section-3').get_text()
+                salary = article_tag.find('span', class_ = 'bloko-header-section-3').get_text().replace(" ", '.')
             else:
                 salary = 'не указана'
 
-            temp_res.append({
+            result.append({
                 'ссылка':link,
                 'вилка зп':salary,
                 'название компании':company,
                 'город':address
             })
-            result.append(temp_res)
         time.sleep(0.33)
-        pages +=1
-        if pages >= 1:
+
+        if len(articles_tags) >= 20:
+            pages +=1
+        else:
             break
-
-
-
-
 
     return result
 
 if __name__ == '__main__':
     res = main()
-
+    print(len(res))
     write_json(res)
